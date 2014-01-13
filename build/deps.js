a61b36887a6385046ef655d1f6f603f80177f1a5
Manuel Baclet <manuel@edagames.com>
2014-01-13 23:46:49 +0100

    Update to support navigate handler.

--- build/deps.js
+++ build/deps.js
@@ -64,7 +64,8 @@ var deps = {
 		src: [
 			'edit/EditToolbar.js',
 			'edit/handler/EditToolbar.Edit.js',
-			'edit/handler/EditToolbar.Delete.js'
+			'edit/handler/EditToolbar.Delete.js',
+			'edit/handler/EditToolbar.Navigate.js'
 		],
 		desc: 'Edit toolbar.',
 		deps: ['EditHandlers', 'CommonUI']
