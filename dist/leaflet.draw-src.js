2dc812b6bbc17ec2bad80fd1e473dc953518de90
Manuel Baclet <manuel@edagames.com>
2014-04-11 17:38:46 +0200

    New tagged build.

--- dist/leaflet.draw-src.js
+++ dist/leaflet.draw-src.js
@@ -2386,6 +2386,96 @@ L.Polygon.include({
 	}
 });
 
+L.Handler.PolyDrag = L.Handler.extend({
+    initialize: function (poly) {
+        this._poly = poly;
+    },
+
+    addHooks: function () {
+        var container = this._poly._container;
+        if (!this._draggable) {
+            this._draggable = new L.Draggable(container, container);
+		}
+
+        this._draggable.on({
+                dragstart: this._onDragStart,
+                drag: this._onDrag,
+                dragend: this._onDragEnd
+            }, this).enable();
+
+        L.DomUtil.addClass(container, 'leaflet-polyline-draggable');
+    },
+
+    removeHooks: function () {
+	    this._draggable.off({
+	        dragstart: this._onDragStart,
+	        drag: this._onDrag,
+	        dragend: this._onDragEnd
+		}, this).disable();
+
+        L.DomUtil.removeClass(this._poly._container, 'leaflet-polyline-draggable');
+    },
+
+    moved: function () {
+        return this._draggable && this._draggable._moved;
+    },
+
+    _onDragStart: function () {
+        if (this._poly.editing.enabled()) {
+            this._wasEditing = true;
+            this._poly.editing.disable();
+        }
+        var map = this._poly._map;
+        map.fire('polyDragStart');
+        L.DomUtil.setPosition(this._poly._container, new L.Point(0, 0));
+        this._poly
+            .fire('movestart')
+            .fire('dragstart');
+    },
+
+    _onDrag: function () {
+        this._poly
+            .fire('move')
+            .fire('drag');
+    },
+
+    _onDragEnd: function (e) {
+        var map = this._poly._map;
+        var oldLatLngs = this._poly.getLatLngs();
+        var newLatLngs = [];
+        var i;
+        for (i in oldLatLngs) {
+            var oldContainerPoint = map.latLngToContainerPoint(oldLatLngs[i]);
+            var newContainerPoint =
+                oldContainerPoint.add(e.target._newPos.subtract(e.target._startPos));
+            newLatLngs.push(map.containerPointToLatLng(newContainerPoint));
+        }
+        L.DomUtil.setPosition(this._poly._container, new L.Point(0, 0));
+        this._poly.setLatLngs(newLatLngs);
+        if (this._wasEditing) {
+            this._poly.editing.enable();
+            this._wasEditing = false;
+            this._poly.edited = true;
+        }
+        this._poly
+            .fire('moveend')
+            .fire('dragend');
+        map.fire('polyDragEnd');
+    }
+});
+
+L.Polyline.addInitHook(function () {
+
+	// Check to see if handler has already been initialized. This is to support versions of Leaflet that still have L.Handler.PolyEdit
+	if (this.dragging) {
+		return;
+	}
+
+	if (L.Handler.PolyDrag) {
+		this.dragging = new L.Handler.PolyDrag(this);
+	}
+});
+
 L.Control.Draw = L.Control.extend({
 
 	options: {
@@ -3307,6 +3397,12 @@ L.EditToolbar.Edit = L.Handler.extend({
 			layer.on('dragend', this._onMarkerDragEnd);
 		} else {
 			layer.editing.enable();
+			if (!(layer instanceof L.Rectangle)) {
+				if (!layer.dragging) {
+					layer.dragging = new L.Handler.PolyDrag(layer);
+				}
+				layer.dragging.enable();
+			}
 		}
 	},
 
@@ -3331,6 +3427,9 @@ L.EditToolbar.Edit = L.Handler.extend({
 			layer.off('dragend', this._onMarkerDragEnd, this);
 		} else {
 			layer.editing.disable();
+			if (!(layer instanceof L.Rectangle)) {
+				layer.dragging.disable();
+			}
 		}
 	},
 
