80b61da2322a4aeae29e9cf4bc900f7a01f98b61
Manuel Baclet <manuel@edagames.com>
2014-06-13 15:21:04 +0200

    New build, new version number.

--- dist/leaflet.draw-src.js
+++ dist/leaflet.draw-src.js
@@ -1552,7 +1552,7 @@ L.Draw.Marker = L.Draw.Feature.extend({
 			var self = this;
 			this.globalDrawLayer.eachLayer(
 				function (layer) {
-					if (layer.edtable) {
+					if (layer.editable) {
 						layer.eachLayer(self._disableDrag, self);
 						layer.off('layeradd', self._enableDrag, self);
 					}
