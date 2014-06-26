e162d04a487d270fc72f144f0a4b96a690a55258
Manuel Baclet <manuel@edagames.com>
2014-06-26 15:51:40 +0200

    New build, new version number.

--- dist/leaflet.draw-src.js
+++ dist/leaflet.draw-src.js
@@ -256,7 +256,7 @@ L.Draw.Feature = L.Handler.extend({
 
 L.Draw.Polyline = L.Draw.Feature.extend({
 	statics: {
-		TYPE: 'polyline'
+		TYPE: 'line'
 	},
 
 	Poly: L.Polyline,
@@ -520,7 +520,7 @@ L.Draw.Polyline = L.Draw.Feature.extend({
 		if (layer && this._markers.length === 0) {
 			var bool;
 
-			if (this.type === 'polyline') {
+			if (this.type === 'line') {
 				bool = layer instanceof L.Polyline && !(layer instanceof L.Polygon) ||
 				layer instanceof L.MultiPolyline;
 			} else {
